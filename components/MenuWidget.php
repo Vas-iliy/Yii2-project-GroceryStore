c5cc982c94ae1d90add5e5483bb339b01385be0c
Vasiliy <kkolyasev1228@gmail.com>
2022-01-19 16:39:19 +0400

    menu widget

--- components/MenuWidget.php
+++ components/MenuWidget.php
@@ -2,6 +2,7 @@
 
 namespace app\components;
 
+use app\models\Category;
 use yii\base\Widget;
 
 class MenuWidget extends Widget
@@ -26,6 +27,42 @@ class MenuWidget extends Widget
 
     public function run()
     {
-        return $this->tpl;
+        $menu = \Yii::$app->cache->get('menu');
+        if ($menu) {
+            return $menu;
+        }
+        $this->data = Category::find()->select('id, parent_id, title')->indexBy('id')->asArray()->all();
+        $this->tree = $this->getTree();
+        $this->menuHtml = '<ul class="' . $this->ul_class . '">';
+        $this->menuHtml .= $this->getMenuHtml($this->tree);
+        $this->menuHtml .= '</ul>';
+        \Yii::$app->cache->set('menu', $this->menuHtml, 3600*24*7);
+        return $this->menuHtml;
+    }
+
+    protected function getTree()
+    {
+        $tree = [];
+        foreach ($this->data as $id => &$node) {
+            if (!$node['parent_id']) $tree[$id] = &$node;
+            else $this->data[$node['parent_id']]['children'][$node['id']] = &$node;
+        }
+        return $tree;
+    }
+
+    protected function getMenuHtml($tree)
+    {
+        $str = '';
+        foreach ($tree as $category) {
+            $str .= $this->catToTemplate($category);
+        }
+        return $str;
+    }
+
+    protected function catToTemplate($category)
+    {
+        ob_start();
+        include __DIR__ . '/menu_tpl/' . $this->tpl;
+        return ob_get_clean();
     }
 }
