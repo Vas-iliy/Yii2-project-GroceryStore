fc4fb8147d228a6fe518f8b435fb4d83cf777a33
Vasiliy <kkolyasev1228@gmail.com>
2022-01-20 13:55:55 +0400

    start cart

--- web/js/main.js
+++ web/js/main.js
@@ -65,3 +65,22 @@ paypal.minicart.cart.on('checkout', function (evt) {
         evt.preventDefault();
     }
 });
+
+/*cart*/
+$('.add-to-cart').on('click', function () {
+   let id = $(this).data('id');
+   $.ajax({
+       url: 'cart/add',
+       data: {id: id},
+       type: 'GET',
+       success: function (res) {
+           console.log(res);
+       }
+       ,
+       error: function () {
+            alert('Error')
+       },
+   });
+   return false;
+});
+/*cart*/
