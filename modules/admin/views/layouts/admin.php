0e5e24bdd7762bbd8e9e7568ee2b2153eea84805
Vasiliy <kkolyasev1228@gmail.com>
2022-01-21 15:23:11 +0400

    AUTH

--- modules/admin/views/layouts/admin.php
+++ modules/admin/views/layouts/admin.php
@@ -266,6 +266,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <!-- Main content -->
         <div class="content">
             <div class="container-fluid">
+                <? debug(Yii::$app->user->identity); ?>
                 <?=$content?>
             </div><!-- /.container-fluid -->
         </div>
