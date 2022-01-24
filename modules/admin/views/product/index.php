b8efccda0f68c5e0b2464689c913b4dcdf139255
Vasiliy <kkolyasev1228@gmail.com>
2022-01-24 10:16:04 +0400

    start products admin

---
+++ modules/admin/views/product/index.php
@@ -0,0 +1,58 @@
+<?php
+
+use app\modules\admin\models\Product;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\modules\admin\models\ProductSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Products';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="product-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Product', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php  //echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            //'id',
+            'title',
+            //'category_id',
+            [
+                'attribute' => 'category_id',
+                'value' => function($data) {
+                    return $data->category->title;
+                },
+            ],
+            //'content:ntext',
+            'price',
+            'old_price',
+            //'description',
+            //'keywords',
+            //'img',
+            //'is_offer',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Product $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
